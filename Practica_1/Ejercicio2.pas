046c77e3b966f30dd7c3da2ca4ea82608c6ed9f4
Candela-A-Arias <candela.arias@alu.ing.unlp.edu.ar>
2026-04-05 19:11:26 -0300

    correcion de la practica 1 y practica 2 completa

--- Practica_1/Ejercicio2.pas
+++ Practica_1/Ejercicio2.pas
@@ -3,6 +3,78 @@ Program Ejercicio2;
 
 Const 
     F =   0; {Constante para indicar el fin de la entrada de n£meros}
+
+Type 
+    {Tipo de archivo binario para almacenar n£meros}
+    Archivo =   file Of integer;
+
+
+{Procedimiento para ingresar n£meros aleatorios en un archivo binario}
+Procedure IngresarNumeros(Var fileA: Archivo);
+
+Var 
+    num:   array[1..10] Of integer;
+    i :   integer;
 Begin
-    writeln('Hola Mundo');
+    num[1] := 22;
+    num[2] := 83;
+    num[3] := 400;
+    num[4] := 5;
+    num[5] := 60;
+    num[6] := 78;
+    num[7] := 8;
+    num[8] := 29;
+    num[9] := 190;
+    num[10] := 52;
+    For i := 1 To 10 Do
+        Begin
+            write(fileA, num[i]);
+        End;
+End;
+
+Procedure MaximoyMinimo(Var fileA: Archivo; Var max, min: integer);
+
+Var 
+    num, i:   integer;
+Begin
+    i := 1;
+    writeln('cod. de Localidad | cantidad de votos');
+    While Not Eof(fileA) Do
+        Begin
+            read(fileA, num);
+            If num > max Then
+                max := num;
+            If num < min Then
+                min := num;
+            If i <= 10 Then
+                Begin
+                    writeln('         ', i, '          |         ', num, '         ');
+                    i := i + 1;
+                End;
+        End;
+End;
+
+
+
+Var 
+    ArchivoVotacion:   Archivo;
+    nombreArchivo:   String;
+    max, min:   integer;
+Begin
+    writeln('ingrese el nombre del archivo a leer: ');
+    nombreArchivo := 'votacion.dat';
+    assign(ArchivoVotacion, nombreArchivo);
+    rewrite(ArchivoVotacion);
+    IngresarNumeros(ArchivoVotacion);
+    close(ArchivoVotacion);
+    writeln('ingrese el nombre del archivo a leer: ');
+    readln(nombreArchivo);
+    assign(ArchivoVotacion, nombreArchivo);
+    reset(ArchivoVotacion);
+    max := -1; {Inicializamos max con un valor menor al m¡nimo posible}
+    min := 9999; {Inicializamos min con un valor mayor al m ximo posible}
+    MaximoyMinimo(ArchivoVotacion, max, min);
+    writeln('El n£mero m ximo de votos es: ', max);
+    writeln('El n£mero m¡nimo de votos es: ', min);
+    close(ArchivoVotacion);
 End.
--- Practica_1/MaterialesBinarios.pas
+++ Practica_1/MaterialesBinarios.pas
@@ -2,97 +2,98 @@
 Program stringesPractica1;
 
 Const 
-  F = 'fin'; {Constante para indicar el fin de la entrada de stringes}
+    F =   'fin'; {Constante para indicar el fin de la entrada de stringes}
 
 Type 
    {Tipo de archivo binario para almacenar stringes}
-  Nodo = Record
-    dato: string;
-    sig: ^Nodo;
-  End;
-  List = ^Nodo; {Puntero para manejar una lista de stringes}
-  Archivo = file Of string;
+    Nodo =   Record
+        dato:   string;
+        sig:   ^Nodo;
+    End;
+    List =   ^Nodo; {Puntero para manejar una lista de stringes}
+    Archivo =   file Of string;
 
 Procedure agregarALista( Var L: List; mat: String);
 
 Var 
-  aux: List;
+    aux:   List;
+
 Begin
-  new(aux);
-  aux^.dato := mat;
-  aux^.sig := L;
-  L := aux;
+    new(aux);
+    aux^.dato := mat;
+    aux^.sig := L;
+    L := aux;
 End;
 
 Procedure Ingresarstringes(Var l: List);
 
 Var 
-  materiales : string;
+    materiales :   string;
 Begin
-  writeln('Ingrese el nombbre del string, o fin para terminar');
-  readln(materiales);
-  While materiales <> F Do
-    Begin
-      agregarALista(l, materiales);
-      readln(materiales);
-    End;
+    writeln('Ingrese el nombbre del string, o fin para terminar');
+    readln(materiales);
+    While materiales <> F Do
+        Begin
+            agregarALista(l, materiales);
+            readln(materiales);
+        End;
 End;
 
 Procedure Guardarstringes(l: List; Var fileC: Archivo);
 
 Var 
-  m : string;
+    m :   string;
 Begin
-  While (l <> Nil) Do
-    Begin
-      m := l^.dato;
-      write(fileC, m);
-      l := l^.sig;
-    End;
+    While (l <> Nil) Do
+        Begin
+            m := l^.dato;
+            write(fileC, m);
+            l := l^.sig;
+        End;
 End;
 
 Procedure MostrarArchivos(nombreArchivo: String);
 
 Var 
-  fileC: Archivo;
-  m: string;
+    fileC:   Archivo;
+    m:   string;
 Begin
-  assign(fileC, nombreArchivo);
-  reset(fileC);
-  writeln('Contenido del archivo ', nombreArchivo, ':');
-  While Not eof(fileC) Do
-    Begin
-      read(fileC, m);
-      writeln('Nombre del material: ', m);
-    End;
-  close(fileC);
+    assign(fileC, nombreArchivo);
+    reset(fileC);
+    writeln('Contenido del archivo ', nombreArchivo, ':');
+    While Not eof(fileC) Do
+        Begin
+            read(fileC, m);
+            writeln('Nombre del material: ', m);
+        End;
+    close(fileC);
 End;
 
 {Programa Principal}
 
 Var 
-  Construccion: Archivo;
+    Construccion:   Archivo;
   {Variable para manejar el archivo binario de stringes}
-  nombreArchivo: String; {Para llamar al archivo binario}
-  L : list; {Lista para almacenar los stringes ingresados por el usuario}
+    nombreArchivo:   String; {Para llamar al archivo binario}
+    L :   list; {Lista para almacenar los stringes ingresados por el usuario}
 
 
 Begin
-  L := Nil; {Inicializar la lista de stringes}
-  writeln('Ingrese el nombre del archivo binario para almacenar los materiales:'
-  );
-  readln(nombreArchivo);
-  assign(Construccion, nombreArchivo);
-  rewrite(Construccion);
-  Ingresarstringes(L);
+    L := Nil; {Inicializar la lista de stringes}
+    writeln('Ingrese el nombre del archivo binario para almacenar los materiales:'
+    );
+    readln(nombreArchivo);
+    assign(Construccion, nombreArchivo);
+    rewrite(Construccion);
+    Ingresarstringes(L);
   {Llamar al procedimiento para ingresar stringes y almacenarlos en la lista}
-  Guardarstringes(L, Construccion);
+    Guardarstringes(L, Construccion);
 
 
 
 
 {Llamar al procedimiento para guardar los stringes de la lista en el archivo binario}
-  close(Construccion);
-  MostrarArchivos(nombreArchivo);
-  readln; {Esperar a que el usuario presione Enter antes de cerrar la consola}
+    close(Construccion);
+    MostrarArchivos(nombreArchivo);
+    readln; {Esperar a que el usuario presione Enter antes de cerrar la consola}
 End.
