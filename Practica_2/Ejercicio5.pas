71e4eb1e6058050ea9399ba1e00f2ca5ad23372c
Candela-A-Arias <candela.arias@alu.ing.unlp.edu.ar>
2026-05-14 14:42:31 -0300

    Por si no prende, ejercicio 5 planteado

---
+++ Practica_2/Ejercicio5.pas
@@ -0,0 +1,21 @@
+
+Program Ejercicio5;
+
+Type 
+    informacion =   Record
+        //primer orden
+        partido:   string;
+        //segundo orden
+        localidad:   string;
+        barrio:   string;
+        cantidad_ni¤os:   integer;
+        cantidad_adultos_mayores:   integer;
+    End;
+
+    municipio =   file Of informacion;
+
+
+Var 
+Begin
+
+End.
